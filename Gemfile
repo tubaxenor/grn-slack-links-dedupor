816ae06652e550dd579d596f10d1b4bacc74819f
Xenor Chang <tubaxenor@gmail.com>
2016-03-04 00:12:21 +0800

    Init commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+
+ruby "2.2.4"
+
+gem "slack-ruby-client", "~> 0.6.0"
+gem "httparty"
+gem "eventmachine"
+gem "faye-websocket"
+
---
+++ bot.rb
@@ -0,0 +1,46 @@
+require "slack-ruby-client"
+require "uri"
+require "httparty"
+require "json"
+
+Slack.configure do |config|
+  config.token = "xoxb-23730056647-auhmwtQU6tEcjbdv9LASaP7X"
+end
+
+client = Slack::RealTime::Client.new
+
+client.on :hello do
+  puts "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
+end
+
+channel_id = client.web_client.channels_info(channel: '#links').channel.id
+github_search_api_base = "https://api.github.com/search/code"
+
+client.on :message do |data|
+  if data.channel == channel_id
+    if (urls = URI.extract(data.text)).any?
+      urls.each do |url|
+        res = HTTParty.get(github_search_api_base,
+          query: { q: url + "+repo:greenruby/grn-static"}
+        ).body
+
+        if (json = JSON.parse(res))["total_count"] > 0
+          occurrences = []
+          json["items"].each do |item|
+            if match = item["name"].match(/\Agrn-(\d+).yml\Z/)
+              occurrences << match[1]
+            end
+          end
+          client.message channel: data.channel, text: "#{url} already posted in #{occurrences.map { |o| "GRN-#{o}" }.join(", ")}"
+        end
+      end
+    end
+  end
+end
+
+client.on :close do |_data|
+  puts 'Connection closed, exiting.'
+  EM.stop
+end
+
+client.start!
